81b3b1799132b7a604e9459e4d30ce8c51ecb9dd
Andrei Catanoiu <practicant.a.catanoiu@techwise.ro>
2026-06-03 21:19:35 +0300

    Add main application loop to log messages every second

--- src/main.c
+++ src/main.c
@@ -1,4 +1,10 @@
+#include "esp_err.h"
+#include "esp_log.h"
+
 void app_main(void)
 {
-    
+    while (1) {
+        ESP_LOGI("APP_MAIN", "Hello, ESP32!");
+        vTaskDelay(pdMS_TO_TICKS(1000)); 
+    }      
 }
